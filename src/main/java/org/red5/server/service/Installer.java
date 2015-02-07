cfce3c7f9501edfcf4954ae0f2ac4d232d2a3d14
Paul Gregoire <mondain@gmail.com>
2015-02-06 22:06:55 -0500

    Updated copyright and url

--- src/main/java/org/red5/server/service/Installer.java
+++ src/main/java/org/red5/server/service/Installer.java
@@ -1,5 +1,5 @@
 /*
- * RED5 Open Source Flash Server - http://code.google.com/p/red5/
+ * RED5 Open Source Flash Server - https://github.com/Red5/
  * 
  * Copyright (c) 2006-2011 by respective authors (see below). All rights reserved.
  * 
