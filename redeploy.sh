6333c34c8121a63c4a882c75bc7eb2a3cca1df43
mekya <ahmetmermerkaya@gmail.com>
2017-07-10 17:36:10 +0300

    Change the target name ant-media-server

--- redeploy.sh
+++ redeploy.sh
@@ -2,14 +2,14 @@
 
 mvn install -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
 
-RED5_DIR=../../softwares/red5-server
+RED5_DIR=../../softwares/ant-media-server
 
-RED5_JAR=./target/red5-server-1.0.9-M2.jar
+RED5_JAR=./target/ant-media-server-1.0.9-M2.jar
 
 SRC_CONF_DIR=./src/main/server/conf/
 
 #copy red5 jar from target dir to red5 dir
-cp  $RED5_JAR  $RED5_DIR/red5-server.jar
+cp  $RED5_JAR  $RED5_DIR/ant-media-server.jar
 
 cp -rf $SRC_CONF_DIR   $RED5_DIR/conf
 
