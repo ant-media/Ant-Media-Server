5718406302d4950ad3294340420ac2afe12a913b
mekya <ahmetmermerkaya@gmail.com>
2018-01-13 18:36:12 +0300

    Check null in ExpireStreamPublishSecurity
    If stream is not in data store do not check expire duration

--- src/main/java/io/antmedia/security/ExpireStreamPublishSecurity.java
+++ src/main/java/io/antmedia/security/ExpireStreamPublishSecurity.java
@@ -9,38 +9,45 @@ import io.antmedia.datastore.db.types.Broadcast;
 public class ExpireStreamPublishSecurity implements IStreamPublishSecurity {
 
 	private IDataStore dataStore;
-	
 
-	
+
+
 	@Override
 	public boolean isPublishAllowed(IScope scope, String name, String mode) {
-		
+
 		boolean result = false;
+
 		Broadcast broadcast = dataStore.get(name);
-		int expireDurationMS = broadcast.getExpireDurationMS();
-		
-		if (expireDurationMS != 0) 
+		if (broadcast != null) 
 		{
-			if (System.currentTimeMillis() < (broadcast.getDate() + expireDurationMS)) {
+			int expireDurationMS = broadcast.getExpireDurationMS();
+
+			if (expireDurationMS != 0) 
+			{
+				if (System.currentTimeMillis() < (broadcast.getDate() + expireDurationMS)) {
+					result = true;
+				}
+			}
+			else {
 				result = true;
 			}
 		}
 		else {
 			result = true;
 		}
-		
+
 		return result;
 	}
-	
-	
+
+
 	public IDataStore getDataStore() {
 		return dataStore;
 	}
-	
-	
+
+
 	public void setDataStore(IDataStore dataStore) {
 		this.dataStore = dataStore;
 	}
-	
+
 
 }
