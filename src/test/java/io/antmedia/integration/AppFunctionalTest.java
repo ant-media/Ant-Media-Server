ef88f07e22bbde301ed4a9798c2245ce84179f76
Davut CAVDAR <davutcavdar@gmail.com>
2018-05-16 17:09:51 +0300

    add integration tests

--- src/test/java/io/antmedia/integration/AppFunctionalTest.java
+++ src/test/java/io/antmedia/integration/AppFunctionalTest.java
@@ -1,36 +1,75 @@
 package io.antmedia.integration;
 
+import static org.bytedeco.javacpp.avformat.av_register_all;
+import static org.bytedeco.javacpp.avformat.avformat_network_init;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.red5.server.stream.ClientBroadcastStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import com.google.gson.Gson;
 
 import io.antmedia.AntMediaApplicationAdapter;
+import io.antmedia.AppSettings;
 import io.antmedia.datastore.db.types.Broadcast;
 import io.antmedia.rest.BroadcastRestService;
 import io.antmedia.rest.BroadcastRestService.BroadcastStatistics;
 import io.antmedia.rest.BroadcastRestService.LiveStatistics;
+import io.antmedia.rest.model.Result;
 import io.antmedia.test.Application;
 
 public class AppFunctionalTest {
 
 	private BroadcastRestService restService = null;
+	private AppSettings appSettings;
+	private static final Logger log = LoggerFactory.getLogger(AppFunctionalTest.class);
+	private static final String SERVER_ADDR = "127.0.0.1"; 
+
 	public static Process process;
+	private static Process tmpExec;
+	private static String ROOT_SERVICE_URL;
 
 	public static final int MAC_OS_X = 0;
 	public static final int LINUX = 1;
 	public static final int WINDOWS = 2;
+	private static BasicCookieStore httpCookieStore;
+	static {
+
+		try {
+			ROOT_SERVICE_URL = "http://" + InetAddress.getLocalHost().getHostAddress() + ":5080/ConsoleApp/rest";
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		log.info("ROOT SERVICE URL: " + ROOT_SERVICE_URL);
+
+	}
+
 	private static int OS_TYPE;
 	private static String ffmpegPath = "ffmpeg";
 	static {
@@ -53,16 +92,33 @@ public class AppFunctionalTest {
 		if (OS_TYPE == MAC_OS_X) {
 			ffmpegPath = "/usr/local/bin/ffmpeg";
 		}
+		av_register_all();
+		avformat_network_init();
 	}
 
 	@Before
 	public void before() {
 		restService = new BroadcastRestService();
+
+		File webApps = new File("webapps");
+		if (!webApps.exists()) {
+			webApps.mkdirs();
+		}
+		File junit = new File(webApps, "junit");
+		if (!junit.exists()) {
+			junit.mkdirs();
+		}
 	}
+	
 
 	@After
 	public void after() {
 		restService = null;
+		try {
+			delete(new File("webapps"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
@@ -80,7 +136,7 @@ public class AppFunctionalTest {
 
 		Broadcast broadcast = restService.createBroadcast("TOBB Demo");
 
-		System.out.println("broadcast id:" + broadcast.getStreamId());
+		log.info("broadcast id:{}", broadcast.getStreamId());
 
 	}
 
@@ -94,6 +150,86 @@ public class AppFunctionalTest {
 		 * AntMediaApplicationAdapter.HOOK_ACTION_END_LIVE_STREAM);
 		 * System.out.println("Result: " + notifyHook.toString());
 		 */
+	}
+
+	@Test
+	public void testSetUpEndPoints() {
+
+		try {
+			RestServiceTest restService = new RestServiceTest();
+
+			Broadcast source=restService.createBroadcast("source_stream");
+			Broadcast endpoint=restService.createBroadcast("endpoint_stream");
+
+			restService.addEndpoint(source.getStreamId(), endpoint.getRtmpURL());
+
+			Thread.sleep(1000);
+
+			assertNotNull(restService.getBroadcast(source.getStreamId()).getEndPointList());
+
+			Process rtmpSendingProcess = execute(ffmpegPath
+					+ " -re -i src/test/resources/test.flv  -codec copy -f flv rtmp://127.0.0.1/LiveApp/"
+					+ source.getStreamId());
+
+			//wait for fetching stream
+			Thread.sleep(5000);
+
+			rtmpSendingProcess.destroy();
+
+			//wait for creating mp4 files
+			Thread.sleep(3000);
+
+			//test mp4 files
+			assertTrue(MuxingTest.testFile("http://" + SERVER_ADDR + ":5080/LiveApp/streams/" + source.getStreamId() + ".mp4"));
+			assertTrue(MuxingTest.testFile("http://" + SERVER_ADDR + ":5080/LiveApp/streams/" + endpoint.getStreamId() + ".mp4"));
+
+			restService.deleteBroadcast(source.getStreamId());
+			restService.deleteBroadcast(endpoint.getStreamId());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+
+	@Test
+	public void testSendRTMPStream() {
+
+		try {
+
+			RestServiceTest rest = new RestServiceTest();
+
+			Broadcast broadcast=rest.createBroadcast("RTMP_stream");
+
+			Process rtmpSendingProcess = execute(ffmpegPath
+					+ " -re -i src/test/resources/test.flv  -codec copy -f flv rtmp://127.0.0.1/LiveApp/"
+					+ broadcast.getStreamId());
+
+			//wait for fetching stream
+			Thread.sleep(5000);
+
+			rtmpSendingProcess.destroy();
+
+			//wait for creating  files
+			Thread.sleep(3000);
+
+			assertTrue(MuxingTest.testFile("http://" + SERVER_ADDR + ":5080/LiveApp/streams/" + broadcast.getStreamId() + ".mp4"));
+			assertTrue(MuxingTest.testFile("http://" + SERVER_ADDR + ":5080/LiveApp/streams/" + broadcast.getStreamId() + ".m3u8"));
+
+
+			if(!BroadcastRestService.isEnterprise()) {
+				return;
+			}
+
+			assertTrue(MuxingTest.testFile("http://" + SERVER_ADDR + ":5080/LiveApp/streams/" + broadcast.getStreamId() + "_240p.m3u8"));
+
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -307,5 +443,99 @@ public class AppFunctionalTest {
 			return false;
 		}
 	}
+	public static void delete(File file)
+			throws IOException{
+
+		if(file.isDirectory()){
+
+			//directory is empty, then delete it
+			if(file.list().length==0){
+
+				file.delete();
+				//System.out.println("Directory is deleted : " 
+				//	+ file.getAbsolutePath());
+
+			}else{
+
+				//list all the directory contents
+				String files[] = file.list();
+
+				for (String temp : files) {
+					//construct the file structure
+					File fileDelete = new File(file, temp);
+
+					//recursive delete
+					delete(fileDelete);
+				}
+
+				//check the directory again, if empty then delete it
+				if(file.list().length==0){
+					file.delete();
+					//System.out.println("Directory is deleted : " 
+					//		+ file.getAbsolutePath());
+				}
+			}
+
+		}else{
+			//if file, then delete it
+			file.delete();
+			//System.out.println("File is deleted : " + file.getAbsolutePath());
+		}
+	}
+
+	public static Process execute(final String command) {
+		tmpExec = null;
+		new Thread() {
+			public void run() {
+				try {
+
+					tmpExec = Runtime.getRuntime().exec(command);
+					InputStream errorStream = tmpExec.getErrorStream();
+					byte[] data = new byte[1024];
+					int length = 0;
+
+					while ((length = errorStream.read(data, 0, data.length)) > 0) {
+						System.out.println(new String(data, 0, length));
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			};
+		}.start();
+
+		while (tmpExec == null) {
+			try {
+				System.out.println("Waiting for exec get initialized...");
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return tmpExec;
+	}
+
+	public Result callIsEnterpriseEdition() throws Exception {
+		String url = "localhost:5080/ConsoleApp/rest/isEnterpriseEdition";
+
+		HttpClient client = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy())
+				.setDefaultCookieStore(httpCookieStore).build();
+		Gson gson = new Gson();
+
+		HttpUriRequest post = RequestBuilder.get().setUri(url).build();
+
+		HttpResponse response = client.execute(post);
+
+		StringBuffer result = RestServiceTest.readResponse(response);
+		if (response.getStatusLine().getStatusCode() != 200) {
+			throw new Exception(result.toString());
+		}
+		System.out.println("result string: " + result.toString());
+		Result tmp = gson.fromJson(result.toString(), Result.class);
+		assertNotNull(tmp);
+		return tmp;
+
+	}
+
 
 }
