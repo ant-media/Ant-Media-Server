2e7addf215a3c2788488b712cd9db0964afa1a2d
Davut CAVDAR <davutcavdar@gmail.com>
2018-05-22 16:51:13 +0300

    update to 1.3.8

--- redeploy.sh
+++ redeploy.sh
@@ -10,7 +10,7 @@ fi
 
 ANT_MEDIA_SERVER_DIR=~/softwares/ant-media-server
 
-ANT_MEDIA_SERVER_JAR=./target/ant-media-server-1.3.5.jar
+ANT_MEDIA_SERVER_JAR=./target/ant-media-server-1.3.8.jar
 
 SRC_CONF_DIR=./src/main/server/conf/
 
