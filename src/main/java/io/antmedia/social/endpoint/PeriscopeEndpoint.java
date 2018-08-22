d0544702b46aa44117b3fe89f26375908b10c42e
mekya <ahmetmermerkaya@gmail.com>
2018-08-22 08:52:44 +0300

    Change the package for ExceptionUtils class

--- src/main/java/io/antmedia/social/endpoint/PeriscopeEndpoint.java
+++ src/main/java/io/antmedia/social/endpoint/PeriscopeEndpoint.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.codehaus.plexus.util.ExceptionUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
