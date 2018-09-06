10eac1d2a6e112eaf3dc18468d00abe1afaa76a7
Davut Cavdar <davutcavdar@gmail.com>
2018-09-06 23:59:19 +0300

    move tokencontrol test

--- src/test/java/io/antmedia/integration/ConsoleAppRestServiceTest.java
+++ src/test/java/io/antmedia/integration/ConsoleAppRestServiceTest.java
@@ -1,5 +1,7 @@
 package io.antmedia.integration;
 
+import static org.bytedeco.javacpp.avformat.av_register_all;
+import static org.bytedeco.javacpp.avformat.avformat_network_init;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
@@ -8,20 +10,28 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.awaitility.Awaitility;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -36,6 +46,7 @@ import com.google.gson.Gson;
 import io.antmedia.AntMediaApplicationAdapter;
 import io.antmedia.EncoderSettings;
 import io.antmedia.datastore.db.types.Broadcast;
+import io.antmedia.datastore.db.types.Token;
 import io.antmedia.rest.BroadcastRestService;
 import io.antmedia.rest.model.AppSettingsModel;
 import io.antmedia.rest.model.Result;
@@ -51,6 +62,11 @@ public class ConsoleAppRestServiceTest {
 
 	private static String TEST_USER_EMAIL = "test@antmedia.io";
 	private static String TEST_USER_PASS = "testtest";
+	private static Process tmpExec;
+	private static final String SERVER_ADDR = "127.0.0.1"; 
+	private static final String SERVICE_URL = "http://localhost:5080/LiveApp/rest";
+	private static Gson gson = new Gson();
+
 
 	private static BasicCookieStore httpCookieStore;
 	private static final Logger log = LoggerFactory.getLogger(ConsoleAppRestServiceTest.class);
@@ -77,6 +93,8 @@ public class ConsoleAppRestServiceTest {
 
 	@Before
 	public void before() {
+		avformat_network_init();
+		av_register_all();
 		httpCookieStore = new BasicCookieStore();
 	}
 
@@ -543,6 +561,114 @@ public class ConsoleAppRestServiceTest {
 
 	}
 	
+	@Test
+	public void testTokenControl() {
+
+		try {
+			// authenticate user
+			User user = new User();
+			user.setEmail(TEST_USER_EMAIL);
+			user.setPassword(TEST_USER_PASS);
+			Result authenticatedUserResult = callAuthenticateUser(user);
+			assertTrue(authenticatedUserResult.isSuccess());
+
+			// get settings from the app
+			AppSettingsModel appSettings = callGetAppSettings("LiveApp");
+
+			appSettings.setTokenControlEnabled(true);
+			appSettings.setMp4MuxingEnabled(true);
+
+
+			Result result = callSetAppSettings("LiveApp", appSettings);
+			assertTrue(result.isSuccess());
+
+			appSettings = callGetAppSettings("LiveApp");
+			assertTrue(appSettings.isTokenControlEnabled());
+
+			Broadcast broadcast = RestServiceTest.callCreateRegularBroadcast();
+			Token accessToken = callGetToken(broadcast.getStreamId());
+			assertNotNull(accessToken);
+
+
+			Process rtmpSendingProcess = execute(ffmpegPath
+					+ " -re -i src/test/resources/test.flv  -codec copy -f flv rtmp://127.0.0.1/LiveApp/"
+					+ broadcast.getStreamId());
+
+
+			//it should be false, because publishing is not allowed and hls files are not created
+			Awaitility.await().pollDelay(5, TimeUnit.SECONDS).atMost(10, TimeUnit.SECONDS).pollInterval(1, TimeUnit.SECONDS).until(() -> {
+				return ConsoleAppRestServiceTest.getStatusCode("http://" + SERVER_ADDR + ":5080/LiveApp/streams/" + broadcast.getStreamId() + ".m3u8?token=" + accessToken.getTokenId())==404;
+			});
+
+			rtmpSendingProcess.destroy();
+			
+			
+			//create token for publishing
+			Token publishToken = callGetToken(broadcast.getStreamId());
+			assertNotNull(publishToken);
+
+			//create token for playing/accessing file
+			Token accessToken2 = callGetToken(broadcast.getStreamId());
+			assertNotNull(accessToken2);
+
+			Process rtmpSendingProcessToken = execute(ffmpegPath
+					+ " -re -i src/test/resources/test.flv  -codec copy -f flv rtmp://127.0.0.1/LiveApp/"
+					+ broadcast.getStreamId()+ "?token=" + publishToken.getTokenId());
+			
+			
+			//it should be false because token control is enabled but no token provided
+
+			Awaitility.await()
+			.pollDelay(5, TimeUnit.SECONDS)
+			.atMost(10, TimeUnit.SECONDS).pollInterval(1, TimeUnit.SECONDS).until(()-> {
+				return  !MuxingTest.testFile("http://" + SERVER_ADDR + ":5080/LiveApp/streams/" 
+						+ broadcast.getStreamId() + ".mp4");
+			});
+
+			rtmpSendingProcessToken.destroy();
+	
+			//this time, it should be true since valid token is provided
+			Awaitility.await().atMost(10, TimeUnit.SECONDS).pollInterval(1, TimeUnit.SECONDS).until(() -> {
+				return MuxingTest.testFile("http://" + SERVER_ADDR + ":5080/LiveApp/streams/" 
+						+ broadcast.getStreamId() + ".mp4?token=" + accessToken2.getTokenId());
+			});
+			
+			appSettings.setTokenControlEnabled(false);
+
+
+			Result flag = callSetAppSettings("LiveApp", appSettings);
+			assertTrue(flag.isSuccess());
+			
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+	}
+	
+	public static Token callGetToken(String streamId) throws Exception {
+		String url = SERVICE_URL + "/broadcast/getToken";
+
+		CloseableHttpClient client = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
+
+		HttpUriRequest get = RequestBuilder.get().setUri(url + "?id=" + streamId + "&expireDate=15784343")
+				.setHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+				.build();
+
+		CloseableHttpResponse response = client.execute(get);
+
+		StringBuffer result = readResponse(response);
+
+		if (response.getStatusLine().getStatusCode() != 200) {
+			throw new Exception(result.toString());
+		}
+		System.out.println("result string: " + result.toString());
+
+		return gson.fromJson(result.toString(), Token.class);
+	}
+	
 	public static int getStatusCode(String url) throws Exception {
 		
 		log.info("url: {}",url);
@@ -701,5 +827,46 @@ public class ConsoleAppRestServiceTest {
 		AppSettingsModel tmp = gson.fromJson(result.toString(), AppSettingsModel.class);
 		assertNotNull(tmp);
 		return tmp;
+	}
+	public static StringBuffer readResponse(HttpResponse response) throws IOException {
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+		return result;
+	}
+	public static Process execute(final String command) {
+		tmpExec = null;
+		new Thread() {
+			public void run() {
+				try {
+
+					tmpExec = Runtime.getRuntime().exec(command);
+					InputStream errorStream = tmpExec.getErrorStream();
+					byte[] data = new byte[1024];
+					int length = 0;
+
+					while ((length = errorStream.read(data, 0, data.length)) > 0) {
+						System.out.println(new String(data, 0, length));
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			};
+		}.start();
+
+		while (tmpExec == null) {
+			try {
+				System.out.println("Waiting for exec get initialized...");
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return tmpExec;
 	}
 }
