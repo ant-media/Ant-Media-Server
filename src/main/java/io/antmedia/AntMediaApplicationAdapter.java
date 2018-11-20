356234d8dc1b790fa946bc9851078a0d5b296b7a
mekya <ahmetmermerkaya@gmail.com>
2018-11-20 15:00:48 +0300

    Remove redundant import

--- src/main/java/io/antmedia/AntMediaApplicationAdapter.java
+++ src/main/java/io/antmedia/AntMediaApplicationAdapter.java
@@ -3,7 +3,6 @@ package io.antmedia;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
