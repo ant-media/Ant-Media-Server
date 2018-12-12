1a2daefb4e9c0b92e4333ee2efd019479cf27fa2
burak-58 <burak@antmedia.io>
2018-12-12 21:21:50 +0300

    fix sonar issues

--- src/main/java/io/antmedia/datastore/AppSettingsManager.java
+++ src/main/java/io/antmedia/datastore/AppSettingsManager.java
@@ -12,10 +12,26 @@ import io.antmedia.datastore.preference.PreferenceStore;
 import io.antmedia.security.AcceptOnlyStreamsInDataStore;
 
 public class AppSettingsManager {
-	
+
+	private static final String SETTINGS_PREVIEW_OVERWRITE = "settings.previewOverwrite";
+	private static final String SETTINGS_ENCODER_SETTINGS_STRING = "settings.encoderSettingsString";
+	private static final String FACEBOOK_CLIENT_ID = "facebook.clientId";
+	private static final String SETTINGS_HLS_PLAY_LIST_TYPE = "settings.hlsPlayListType";
+	private static final String SETTINGS_HLS_TIME = "settings.hlsTime";
+	private static final String SETTINGS_HLS_LIST_SIZE = "settings.hlsListSize";
+	private static final String SETTINGS_VOD_FOLDER = "settings.vodFolder";
+	private static final String SETTINGS_OBJECT_DETECTION_ENABLED = "settings.objectDetectionEnabled";
+	private static final String SETTINGS_HLS_MUXING_ENABLED = "settings.hlsMuxingEnabled";
+	private static final String SETTINGS_ADD_DATE_TIME_TO_MP4_FILE_NAME = "settings.addDateTimeToMp4FileName";
+	private static final String SETTINGS_MP4_MUXING_ENABLED = "settings.mp4MuxingEnabled";
 	private static final String SETTINGS_ACCEPT_ONLY_STREAMS_IN_DATA_STORE = "settings.acceptOnlyStreamsInDataStore";
 
 	private static final Logger log = LoggerFactory.getLogger(AppSettingsManager.class);
+
+	private AppSettingsManager() {
+
+	}
+
 
 	public static boolean updateAppSettings(ApplicationContext applicationContext, AppSettingsModel settingsModel, boolean sendToCluster) {
 		if (!updateAppSettingsFile(applicationContext.getApplicationName(), settingsModel))
@@ -57,12 +73,12 @@ public class AppSettingsManager {
 			AcceptOnlyStreamsInDataStore securityHandler = (AcceptOnlyStreamsInDataStore) applicationContext.getBean(AcceptOnlyStreamsInDataStore.BEAN_NAME);
 			securityHandler.setEnabled(settingsModel.isAcceptOnlyStreamsInDataStore());
 		}
-		
+
 		if(sendToCluster && applicationContext.getParent().containsBean(IClusterNotifier.BEAN_NAME)) {
 			IClusterNotifier cluster = (IClusterNotifier) applicationContext.getParent().getBean(IClusterNotifier.BEAN_NAME);
 			cluster.sendAppSettings(applicationContext.getApplicationName(), settingsModel);
 		}
-		
+
 		return true;
 	}
 
@@ -70,59 +86,59 @@ public class AppSettingsManager {
 		PreferenceStore store = new PreferenceStore("red5-web.properties");
 		store.setFullPath("webapps/"+appName+"/WEB-INF/red5-web.properties");
 
-		store.put("settings.mp4MuxingEnabled", String.valueOf(appsettings.isMp4MuxingEnabled()));
-		store.put("settings.addDateTimeToMp4FileName", String.valueOf(appsettings.isAddDateTimeToMp4FileName()));
-		store.put("settings.hlsMuxingEnabled", String.valueOf(appsettings.isHlsMuxingEnabled()));
+		store.put(SETTINGS_MP4_MUXING_ENABLED, String.valueOf(appsettings.isMp4MuxingEnabled()));
+		store.put(SETTINGS_ADD_DATE_TIME_TO_MP4_FILE_NAME, String.valueOf(appsettings.isAddDateTimeToMp4FileName()));
+		store.put(SETTINGS_HLS_MUXING_ENABLED, String.valueOf(appsettings.isHlsMuxingEnabled()));
 		store.put(SETTINGS_ACCEPT_ONLY_STREAMS_IN_DATA_STORE, String.valueOf(appsettings.isAcceptOnlyStreamsInDataStore()));
-		store.put("settings.objectDetectionEnabled", String.valueOf(appsettings.isObjectDetectionEnabled()));
+		store.put(SETTINGS_OBJECT_DETECTION_ENABLED, String.valueOf(appsettings.isObjectDetectionEnabled()));
 		store.put("settings.tokenControlEnabled", String.valueOf(appsettings.isTokenControlEnabled()));
-		
+
 		if (appsettings.getVodFolder() == null) {
-			store.put("settings.vodFolder", "");
+			store.put(SETTINGS_VOD_FOLDER, "");
 		}else {
-			store.put("settings.vodFolder", appsettings.getVodFolder());
+			store.put(SETTINGS_VOD_FOLDER, appsettings.getVodFolder());
 		}
 
 
 		if (appsettings.getHlsListSize() < 5) {
-			store.put("settings.hlsListSize", "5");
+			store.put(SETTINGS_HLS_LIST_SIZE, "5");
 		}
 		else {
-			store.put("settings.hlsListSize", String.valueOf(appsettings.getHlsListSize()));
+			store.put(SETTINGS_HLS_LIST_SIZE, String.valueOf(appsettings.getHlsListSize()));
 		}
 
 		if (appsettings.getHlsTime() < 2) {
-			store.put("settings.hlsTime", "2");
+			store.put(SETTINGS_HLS_TIME, "2");
 		}
 		else {
-			store.put("settings.hlsTime", String.valueOf(appsettings.getHlsTime()));
+			store.put(SETTINGS_HLS_TIME, String.valueOf(appsettings.getHlsTime()));
 		}
 
 		if (appsettings.getHlsPlayListType() == null) {
-			store.put("settings.hlsPlayListType", "");
+			store.put(SETTINGS_HLS_PLAY_LIST_TYPE, "");
 		}
 		else {
-			store.put("settings.hlsPlayListType", appsettings.getHlsPlayListType());
+			store.put(SETTINGS_HLS_PLAY_LIST_TYPE, appsettings.getHlsPlayListType());
 		}
 
 		if (appsettings.getFacebookClientId() == null){
-			store.put("facebook.clientId", "");
+			store.put(FACEBOOK_CLIENT_ID, "");
 		}
 		else {
-			store.put("facebook.clientId", appsettings.getFacebookClientId());
+			store.put(FACEBOOK_CLIENT_ID, appsettings.getFacebookClientId());
 		}
 
 		if (appsettings.getEncoderSettings() == null) {
-			store.put("settings.encoderSettingsString", "");
+			store.put(SETTINGS_ENCODER_SETTINGS_STRING, "");
 		}
 		else {
-			store.put("settings.encoderSettingsString", io.antmedia.AppSettings.getEncoderSettingsString(appsettings.getEncoderSettings()));
+			store.put(SETTINGS_ENCODER_SETTINGS_STRING, io.antmedia.AppSettings.getEncoderSettingsString(appsettings.getEncoderSettings()));
 		}
 
-		store.put("settings.previewOverwrite", String.valueOf(appsettings.isPreviewOverwrite()));
-				
+		store.put(SETTINGS_PREVIEW_OVERWRITE, String.valueOf(appsettings.isPreviewOverwrite()));
+
 		return store.save();
-		
+
 	}
 
 	public static AppSettingsModel getAppSettings(String appname) {
@@ -130,41 +146,41 @@ public class AppSettingsManager {
 		store.setFullPath("webapps/"+appname+"/WEB-INF/red5-web.properties");
 		AppSettingsModel appSettings = new AppSettingsModel();
 
-		if (store.get("settings.mp4MuxingEnabled") != null) {
-			appSettings.setMp4MuxingEnabled(Boolean.parseBoolean(store.get("settings.mp4MuxingEnabled")));
+		if (store.get(SETTINGS_MP4_MUXING_ENABLED) != null) {
+			appSettings.setMp4MuxingEnabled(Boolean.parseBoolean(store.get(SETTINGS_MP4_MUXING_ENABLED)));
 		}
-		if (store.get("settings.addDateTimeToMp4FileName") != null) {
-			appSettings.setAddDateTimeToMp4FileName(Boolean.parseBoolean(store.get("settings.addDateTimeToMp4FileName")));
+		if (store.get(SETTINGS_ADD_DATE_TIME_TO_MP4_FILE_NAME) != null) {
+			appSettings.setAddDateTimeToMp4FileName(Boolean.parseBoolean(store.get(SETTINGS_ADD_DATE_TIME_TO_MP4_FILE_NAME)));
 		}
-		if (store.get("settings.hlsMuxingEnabled") != null) {
-			appSettings.setHlsMuxingEnabled(Boolean.parseBoolean(store.get("settings.hlsMuxingEnabled")));
+		if (store.get(SETTINGS_HLS_MUXING_ENABLED) != null) {
+			appSettings.setHlsMuxingEnabled(Boolean.parseBoolean(store.get(SETTINGS_HLS_MUXING_ENABLED)));
 		}
-		if (store.get("settings.objectDetectionEnabled") != null) {
-			appSettings.setObjectDetectionEnabled(Boolean.parseBoolean(store.get("settings.objectDetectionEnabled")));
-		}
-		
-		if (store.get("settings.hlsListSize") != null) {
-			appSettings.setHlsListSize(Integer.valueOf(store.get("settings.hlsListSize")));
+		if (store.get(SETTINGS_OBJECT_DETECTION_ENABLED) != null) {
+			appSettings.setObjectDetectionEnabled(Boolean.parseBoolean(store.get(SETTINGS_OBJECT_DETECTION_ENABLED)));
 		}
 
-		if (store.get("settings.hlsTime") != null) {
-			appSettings.setHlsTime(Integer.valueOf(store.get("settings.hlsTime")));
+		if (store.get(SETTINGS_HLS_LIST_SIZE) != null) {
+			appSettings.setHlsListSize(Integer.valueOf(store.get(SETTINGS_HLS_LIST_SIZE)));
 		}
-		appSettings.setHlsPlayListType(store.get("settings.hlsPlayListType"));
-		appSettings.setFacebookClientId(store.get("facebook.clientId"));
+
+		if (store.get(SETTINGS_HLS_TIME) != null) {
+			appSettings.setHlsTime(Integer.valueOf(store.get(SETTINGS_HLS_TIME)));
+		}
+		appSettings.setHlsPlayListType(store.get(SETTINGS_HLS_PLAY_LIST_TYPE));
+		appSettings.setFacebookClientId(store.get(FACEBOOK_CLIENT_ID));
 		appSettings.setFacebookClientSecret(store.get("facebook.clientSecret"));
 		appSettings.setYoutubeClientId(store.get("youtube.clientId"));
 		appSettings.setYoutubeClientSecret(store.get("youtube.clientSecret"));
 		appSettings.setPeriscopeClientId(store.get("periscope.clientId"));
 		appSettings.setPeriscopeClientSecret(store.get("periscope.clientSecret"));
 		appSettings.setAcceptOnlyStreamsInDataStore(Boolean.valueOf(store.get(SETTINGS_ACCEPT_ONLY_STREAMS_IN_DATA_STORE)));
-		appSettings.setVodFolder(store.get("settings.vodFolder"));
+		appSettings.setVodFolder(store.get(SETTINGS_VOD_FOLDER));
 		appSettings.setTokenControlEnabled(Boolean.parseBoolean(store.get("settings.tokenControlEnabled")));
 
-		appSettings.setEncoderSettings(io.antmedia.AppSettings.getEncoderSettingsList(store.get("settings.encoderSettingsString")));
+		appSettings.setEncoderSettings(io.antmedia.AppSettings.getEncoderSettingsList(store.get(SETTINGS_ENCODER_SETTINGS_STRING)));
 
-		if (store.get("settings.previewOverwrite") != null) {
-			appSettings.setPreviewOverwrite(Boolean.parseBoolean(store.get("settings.previewOverwrite")));
+		if (store.get(SETTINGS_PREVIEW_OVERWRITE) != null) {
+			appSettings.setPreviewOverwrite(Boolean.parseBoolean(store.get(SETTINGS_PREVIEW_OVERWRITE)));
 		}
 
 		return appSettings;
