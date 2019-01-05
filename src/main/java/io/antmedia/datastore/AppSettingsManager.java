f5891418beadf0c66ff5727507896d0814e02ece
mekya <ahmetmermerkaya@gmail.com>
2019-01-06 00:24:28 +0300

    Cover more lines to quality pass

--- src/main/java/io/antmedia/datastore/AppSettingsManager.java
+++ src/main/java/io/antmedia/datastore/AppSettingsManager.java
@@ -19,53 +19,55 @@ public class AppSettingsManager {
 	}
 
 	public static boolean updateAppSettings(ApplicationContext applicationContext, AppSettingsModel settingsModel, boolean sendToCluster) {
-		if (!updateAppSettingsFile(applicationContext.getApplicationName(), settingsModel))
+		boolean result = false;
+		if (updateAppSettingsFile(applicationContext.getApplicationName(), settingsModel))
 		{
-			return false;
+			if (applicationContext.containsBean(AcceptOnlyStreamsInDataStore.BEAN_NAME)) {
+				AcceptOnlyStreamsInDataStore securityHandler = (AcceptOnlyStreamsInDataStore) applicationContext.getBean(AcceptOnlyStreamsInDataStore.BEAN_NAME);
+				securityHandler.setEnabled(settingsModel.isAcceptOnlyStreamsInDataStore());
+			}
+			
+			if(sendToCluster && applicationContext.getParent().containsBean(IClusterNotifier.BEAN_NAME)) {
+				IClusterNotifier cluster = (IClusterNotifier) applicationContext.getParent().getBean(IClusterNotifier.BEAN_NAME);
+				cluster.sendAppSettings(applicationContext.getApplicationName(), settingsModel);
+			}
+			
+			if (applicationContext.containsBean(AppSettings.BEAN_NAME)) {
+				AppSettings appSettings = (AppSettings) applicationContext.getBean(AppSettings.BEAN_NAME);
+
+				appSettings.setMp4MuxingEnabled(settingsModel.isMp4MuxingEnabled());
+				appSettings.setAddDateTimeToMp4FileName(settingsModel.isAddDateTimeToMp4FileName());
+				appSettings.setHlsMuxingEnabled(settingsModel.isHlsMuxingEnabled());
+				appSettings.setObjectDetectionEnabled(settingsModel.isObjectDetectionEnabled());
+				appSettings.setHlsListSize(String.valueOf(settingsModel.getHlsListSize()));
+				appSettings.setHlsTime(String.valueOf(settingsModel.getHlsTime()));
+				appSettings.setHlsPlayListType(settingsModel.getHlsPlayListType());
+				appSettings.setAcceptOnlyStreamsInDataStore(settingsModel.isAcceptOnlyStreamsInDataStore());
+				appSettings.setTokenControlEnabled(settingsModel.isTokenControlEnabled());
+				appSettings.setWebRTCEnabled(settingsModel.isWebRTCEnabled());
+				appSettings.setWebRTCFrameRate(settingsModel.getWebRTCFrameRate());
+
+				appSettings.setAdaptiveResolutionList(settingsModel.getEncoderSettings());
+
+				String oldVodFolder = appSettings.getVodFolder();
+
+				appSettings.setVodFolder(settingsModel.getVodFolder());
+				appSettings.setPreviewOverwrite(settingsModel.isPreviewOverwrite());
+
+				AntMediaApplicationAdapter bean = (AntMediaApplicationAdapter) applicationContext.getBean("web.handler");
+
+				bean.synchUserVoDFolder(oldVodFolder, settingsModel.getVodFolder());
+
+				log.warn("app settings updated for {}", applicationContext.getApplicationName());	
+				result = true;
+			}
+			else {
+				log.warn("App{} has no app.settings bean to update ", applicationContext.getApplicationName());
+				result = false;
+			}
 		}
-		if (applicationContext.containsBean(AppSettings.BEAN_NAME)) {
-			AppSettings appSettings = (AppSettings) applicationContext.getBean(AppSettings.BEAN_NAME);
 
-			appSettings.setMp4MuxingEnabled(settingsModel.isMp4MuxingEnabled());
-			appSettings.setAddDateTimeToMp4FileName(settingsModel.isAddDateTimeToMp4FileName());
-			appSettings.setHlsMuxingEnabled(settingsModel.isHlsMuxingEnabled());
-			appSettings.setObjectDetectionEnabled(settingsModel.isObjectDetectionEnabled());
-			appSettings.setHlsListSize(String.valueOf(settingsModel.getHlsListSize()));
-			appSettings.setHlsTime(String.valueOf(settingsModel.getHlsTime()));
-			appSettings.setHlsPlayListType(settingsModel.getHlsPlayListType());
-			appSettings.setAcceptOnlyStreamsInDataStore(settingsModel.isAcceptOnlyStreamsInDataStore());
-			appSettings.setTokenControlEnabled(settingsModel.isTokenControlEnabled());
-			appSettings.setWebRTCEnabled(settingsModel.isWebRTCEnabled());
-			appSettings.setWebRTCFrameRate(settingsModel.getWebRTCFrameRate());
-
-			appSettings.setAdaptiveResolutionList(settingsModel.getEncoderSettings());
-
-			String oldVodFolder = appSettings.getVodFolder();
-
-			appSettings.setVodFolder(settingsModel.getVodFolder());
-			appSettings.setPreviewOverwrite(settingsModel.isPreviewOverwrite());
-
-			AntMediaApplicationAdapter bean = (AntMediaApplicationAdapter) applicationContext.getBean("web.handler");
-
-			bean.synchUserVoDFolder(oldVodFolder, settingsModel.getVodFolder());
-
-			log.warn("app settings updated");	
-		}
-		else {
-			log.warn("App has no app.settings bean");
-			return false;
-		}
-		if (applicationContext.containsBean(AcceptOnlyStreamsInDataStore.BEAN_NAME)) {
-			AcceptOnlyStreamsInDataStore securityHandler = (AcceptOnlyStreamsInDataStore) applicationContext.getBean(AcceptOnlyStreamsInDataStore.BEAN_NAME);
-			securityHandler.setEnabled(settingsModel.isAcceptOnlyStreamsInDataStore());
-		}
-
-		if(sendToCluster && applicationContext.getParent().containsBean(IClusterNotifier.BEAN_NAME)) {
-			IClusterNotifier cluster = (IClusterNotifier) applicationContext.getParent().getBean(IClusterNotifier.BEAN_NAME);
-			cluster.sendAppSettings(applicationContext.getApplicationName(), settingsModel);
-		}
-
-		return true;
+		return result;
 	}
 
 	private static boolean updateAppSettingsFile(String appName, AppSettingsModel appsettings) {
@@ -81,7 +83,7 @@ public class AppSettingsManager {
 		store.put(AppSettings.SETTINGS_WEBRTC_ENABLED, String.valueOf(appsettings.isWebRTCEnabled()));
 		store.put(AppSettings.SETTINGS_WEBRTC_FRAME_RATE, String.valueOf(appsettings.getWebRTCFrameRate()));
 
-		
+
 		if (appsettings.getVodFolder() == null) {
 			store.put(AppSettings.SETTINGS_VOD_FOLDER, "");
 		}else {
@@ -138,11 +140,11 @@ public class AppSettingsManager {
 		if (store.get(AppSettings.SETTINGS_MP4_MUXING_ENABLED) != null) {
 			appSettings.setMp4MuxingEnabled(Boolean.parseBoolean(store.get(AppSettings.SETTINGS_MP4_MUXING_ENABLED)));
 		}
-		
+
 		if (store.get(AppSettings.SETTINGS_WEBRTC_ENABLED) != null) {
 			appSettings.setWebRTCEnabled(Boolean.parseBoolean(store.get(AppSettings.SETTINGS_WEBRTC_ENABLED)));
 		}
-		
+
 		if (store.get(AppSettings.SETTINGS_ADD_DATE_TIME_TO_MP4_FILE_NAME) != null) {
 			appSettings.setAddDateTimeToMp4FileName(Boolean.parseBoolean(store.get(AppSettings.SETTINGS_ADD_DATE_TIME_TO_MP4_FILE_NAME)));
 		}
@@ -160,11 +162,11 @@ public class AppSettingsManager {
 		if (store.get(AppSettings.SETTINGS_HLS_TIME) != null) {
 			appSettings.setHlsTime(Integer.valueOf(store.get(AppSettings.SETTINGS_HLS_TIME)));
 		}
-		
+
 		if (store.get(AppSettings.SETTINGS_WEBRTC_FRAME_RATE) != null) {
 			appSettings.setWebRTCFrameRate(Integer.valueOf(store.get(AppSettings.SETTINGS_WEBRTC_FRAME_RATE)));
 		}
-		
+
 		appSettings.setHlsPlayListType(store.get(AppSettings.SETTINGS_HLS_PLAY_LIST_TYPE));
 		appSettings.setFacebookClientId(store.get(AppSettings.FACEBOOK_CLIENT_ID));
 		appSettings.setFacebookClientSecret(store.get(AppSettings.FACEBOOK_CLIENT_SECRET));
