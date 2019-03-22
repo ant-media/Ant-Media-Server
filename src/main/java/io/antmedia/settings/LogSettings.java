0e4877a66bb9f2bf21c9a86ffcc4735defdd2447
SelimEmre <selimemretoy@gmail.com>
2019-03-22 14:45:33 +0300

    add LogSetting

---
+++ src/main/java/io/antmedia/settings/LogSettings.java
@@ -0,0 +1,22 @@
+package io.antmedia.settings;
+
+public class LogSettings {
+	
+	private String logFileSize = null;
+	private String logLevel = null;
+
+ 	public String getLogFileSize() {
+		return logFileSize;
+	}
+	public void setLogFileSize(String logFileSize) {
+		this.logFileSize = logFileSize;
+	}
+
+ 	public String getLogLevel() {
+		return logLevel;
+	}
+	public void setLogLevel(String logLevel) {
+		this.logLevel = logLevel;
+	}
+
+}
