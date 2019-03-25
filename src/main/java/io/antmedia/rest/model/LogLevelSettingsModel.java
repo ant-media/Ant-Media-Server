4cece02b56094c9be7d15dba35e1ba4549d8371d
SelimEmre <selimemretoy@gmail.com>
2019-03-25 13:27:11 +0300

    LogLevelSettingModel added

---
+++ src/main/java/io/antmedia/rest/model/LogLevelSettingsModel.java
@@ -0,0 +1,13 @@
+package io.antmedia.rest.model;
+
+public class LogLevelSettingsModel {
+
+	private String logLevel;
+	
+	public String getLogLevel() {
+		return logLevel;
+	}
+	public void setLogLevel(String logLevel) {
+		this.logLevel = logLevel;
+	}
+}
