59354b968597cae24001d38ffff06987c0e5462d
SelimEmre <selimemretoy@gmail.com>
2019-04-20 23:36:01 +0300

    Refactor EmailSettings class

---
+++ src/main/java/io/antmedia/settings/EmailSettings.java
@@ -0,0 +1,55 @@
+package io.antmedia.settings;
+
+public class EmailSettings {
+
+	private String emailUsername = null;
+
+	public String getEmailUsername() {
+		return emailUsername;
+	}
+
+	public void setEmailUsername(String emailUsername) {
+		this.emailUsername = emailUsername;
+	}
+
+	private String emailPassword = null;
+
+	public String getEmailPassword() {
+		return emailPassword;
+	}
+
+	public void setEmailPassword(String emailPassword) {
+		this.emailPassword = emailPassword;
+	}
+
+	private String emailSmtpHost = null;
+
+	public String getEmailSmtpHost() {
+		return emailSmtpHost;
+	}
+
+	public void setEmailSmtpHost(String emailSmtpHost) {
+		this.emailSmtpHost = emailSmtpHost;
+	}
+
+	private String emailSmtpEncryption = null;
+
+	public String getEmailSmtpEncryption() {
+		return emailSmtpEncryption;
+	}
+
+	public void setEmailSmtpEncryption(String emailSmtpEncryption) {
+		this.emailSmtpEncryption = emailSmtpEncryption;
+	}
+
+	private String emailCheckDate = null;
+
+	public String getEmailCheckDate() {
+		return emailCheckDate;
+	}
+
+	public void setEmailCheckDate(String emailCheckDate) {
+		this.emailCheckDate = emailCheckDate;
+	}
+
+}
