e6d12b0310cdccfdb2e655757d20302c2aa8500d
SelimEmre <selimemretoy@gmail.com>
2019-04-20 23:17:28 +0300

    add email settings bean and class

---
+++ src/main/java/io/antmedia/settings/EmailSetting.java
@@ -0,0 +1,15 @@
+package io.antmedia.settings;
+
+public class EmailSetting {
+	
+	private String emailUsername = null;
+	
+	private String emailPassword = null;
+	
+	private String emailSmtpHost = null;
+	
+	private String emailSmtpEncryption = null;
+	
+	private String emailCheckDate = null;
+
+}
