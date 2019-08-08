08df147c15246bc950c3f0f299ca3146642b1a51
mekya <ahmetmermerkaya@gmail.com>
2019-08-08 16:34:47 +0300

    Trigger build

--- src/main/java/io/antmedia/filter/CorsHeaderFilter.java
+++ src/main/java/io/antmedia/filter/CorsHeaderFilter.java
@@ -7,8 +7,6 @@ import java.util.Locale;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
