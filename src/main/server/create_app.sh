6965cc35e5d49ce4ef404a51ed18d99d5e0a88da
burak-58 <burak@antmedia.io>
2019-09-02 00:16:35 +0300

    adopt ci to common application

---
+++ src/main/server/create_app.sh
@@ -0,0 +1,21 @@
+APP_NAME=$1
+AMS_DIR=$2
+APP_DIR=$AMS_DIR/webapps/$APP_NAME
+RED5_PROPERTIES_FILE=$APP_DIR/WEB-INF/red5-web.properties
+WEB_XML_FILE=$APP_DIR/WEB-INF/web.xml
+
+
+mkdir $APP_DIR 
+cp $AMS_DIR/StreamApp*.war $APP_DIR
+cd $APP_DIR
+jar -xf StreamApp*.war
+rm StreamApp*.war
+
+sed -i 's^webapp.dbName=.*^webapp.dbName='$APP_NAME'.db^' $RED5_PROPERTIES_FILE
+sed -i 's^webapp.contextPath=.*^webapp.contextPath=/'$APP_NAME'^' $RED5_PROPERTIES_FILE
+sed -i 's^db.app.name=.*^db.app.name='$APP_NAME'^' $RED5_PROPERTIES_FILE
+sed -i 's^db.name=.*^db.name='$APP_NAME'^' $RED5_PROPERTIES_FILE
+
+sed -i 's^<display-name>StreamApp^<display-name>'$APP_NAME'^' $WEB_XML_FILE
+sed -i 's^<param-value>/StreamApp^<param-value>/'$APP_NAME'^' $WEB_XML_FILE
+
