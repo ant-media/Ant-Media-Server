45536f24f56df69e100092d5705add020bdc587b
mekya <ahmetmermerkaya@gmail.com>
2019-10-06 14:09:54 +0300

    Generate swagger doc for v2 only

--- src/main/java/io/antmedia/rest/RestServiceBase.java
+++ src/main/java/io/antmedia/rest/RestServiceBase.java
@@ -10,7 +10,6 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.time.Instant;
 import java.util.ArrayList;
