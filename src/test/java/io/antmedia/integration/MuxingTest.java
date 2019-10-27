4922ed5426563d39c2d3eaa763259c10c7c9411a
burak-58 <burak@antmedia.io>
2019-10-27 20:24:42 +0300

    audio only test added

--- src/test/java/io/antmedia/integration/MuxingTest.java
+++ src/test/java/io/antmedia/integration/MuxingTest.java
@@ -695,5 +695,28 @@ public class MuxingTest {
 
 		rtmpSendingProcess.destroy();
 	}
+	
+	@Test
+	public void testAudioOnlyStreaming() {
+		Process rtmpSendingProcess = null;
+		String streamName = "live_test"  + (int)(Math.random() * 999999);
+
+		try {
+			rtmpSendingProcess = execute(
+					ffmpegPath + " -re -i src/test/resources/test.flv -c copy -vn -f flv rtmp://"
+							+ SERVER_ADDR + "/LiveApp/" + streamName);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		RestServiceTest restService = new RestServiceTest();
+		
+		Awaitility.await().atMost(10, TimeUnit.SECONDS).pollInterval(1, TimeUnit.SECONDS).until(() -> {
+			Broadcast broadcast = restService.getBroadcast(streamName);
+			return broadcast.getQuality() != null;
+		});
+
+		rtmpSendingProcess.destroy();
+	}
 
 }
