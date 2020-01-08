40428415a765868a144671cceda48772f7a565fd
burak-58 <burak@antmedia.io>
2020-01-08 13:15:33 +0300

    no change just for ci

--- src/test/java/io/antmedia/test/db/DBStoresUnitTest.java
+++ src/test/java/io/antmedia/test/db/DBStoresUnitTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
