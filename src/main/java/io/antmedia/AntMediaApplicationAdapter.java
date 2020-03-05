6ffdbcdc4e4638f3fb42964f041a5a6e096d9ef9
burak-58 <burak@antmedia.io>
2020-03-05 17:42:06 +0300

    data channel settings

--- src/main/java/io/antmedia/AntMediaApplicationAdapter.java
+++ src/main/java/io/antmedia/AntMediaApplicationAdapter.java
@@ -1086,7 +1086,8 @@ public class AntMediaApplicationAdapter implements IAntMediaStreamHandler, IShut
 																	: "");
 		store.put(AppSettings.SETTINGS_H264_ENABLED, String.valueOf(appsettings.isH264Enabled()));
 		store.put(AppSettings.SETTINGS_VP8_ENABLED, String.valueOf(appsettings.isVp8Enabled()));
-
+		store.put(AppSettings.SETTINGS_DATA_CHANNEL_ENABLED, String.valueOf(appsettings.isDataChannelEnabled()));
+		store.put(AppSettings.SETTINGS_DATA_CHANNEL_PLAYER_DISTRIBUTION, appsettings.getDataChannelPlayerDistribution());
 
 		return store.save();
 	}
@@ -1124,6 +1125,8 @@ public class AntMediaApplicationAdapter implements IAntMediaStreamHandler, IShut
 		appSettings.setH264Enabled(newSettings.isH264Enabled());
 		appSettings.setVp8Enabled(newSettings.isVp8Enabled());
 
+		appSettings.setDataChannelEnabled(newSettings.isDataChannelEnabled());
+		appSettings.setDataChannelPlayerDistribution(newSettings.getDataChannelPlayerDistribution());
 		
 		logger.warn("app settings updated for {}", getScope().getName());	
 	}
