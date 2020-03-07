8b4adbb0d5b11964be7438a866e955c83802c7fd
mekya <ahmetmermerkaya@gmail.com>
2020-03-07 11:24:35 +0300

    Run audio only test from camera emulator

--- src/test/java/io/antmedia/test/StreamFetcherUnitTest.java
+++ src/test/java/io/antmedia/test/StreamFetcherUnitTest.java
@@ -757,7 +757,9 @@ public class StreamFetcherUnitTest extends AbstractJUnit4SpringContextTests {
 	public void testAudioOnlySource() {
 		logger.info("running testAudioOnlySource");
 		//test AudioOnly Source
-		testFetchStreamSources("rtmp://37.247.100.100/shoutcast/karadenizfm.stream", false, false);
+		startCameraEmulator();
+		testFetchStreamSources("rtsp://127.0.0.1:6554/test_audio_only.flv", false, false);
+		stopCameraEmulator();
 		logger.info("leaving testAudioOnlySource");
 	}
 
