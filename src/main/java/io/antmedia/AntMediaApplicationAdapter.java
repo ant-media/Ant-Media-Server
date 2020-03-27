b9e7f13857fdc72254e9c480ca0028cffd4b5ab9
mekya <ahmetmermerkaya@gmail.com>
2020-03-27 15:10:07 +0300

    Refactor the StreamAcceptFilter

--- src/main/java/io/antmedia/AntMediaApplicationAdapter.java
+++ src/main/java/io/antmedia/AntMediaApplicationAdapter.java
@@ -839,7 +839,7 @@ public class AntMediaApplicationAdapter implements IAntMediaStreamHandler, IShut
 	public void setQualityParameters(String id, String quality, double speed, int pendingPacketSize) {
 		
 		vertx.setTimer(5, h -> {
-			logger.debug("update source quality for stream: {} quality:{} speed:{}", id, quality, speed);
+			logger.info("update source quality for stream: {} quality:{} speed:{}", id, quality, speed);
 			getDataStore().updateSourceQualityParameters(id, quality, speed, pendingPacketSize);
 		});
 	}
