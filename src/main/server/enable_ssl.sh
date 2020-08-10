e7a6933aa446c71548870cb689721de1ecdf0ab3
muratugureminoglu <murat@murat.ws>
2020-08-10 18:44:04 +0300

    Update enable_ssl.sh

--- src/main/server/enable_ssl.sh
+++ src/main/server/enable_ssl.sh
@@ -243,12 +243,14 @@ create_cron_job(){
 
     $SUDO crontab -l > /tmp/cronfile
 
-    if [ $(grep -E "enable_ssl.sh.*$domain" /tmp/cronfile | wc -l) -eq "0" ]; then
-      $SUDO echo "00 03 */85 * * cd $INSTALL_DIRECTORY && ./enable_ssl.sh -d $domain -r" >> /tmp/cronfile
-      $SUDO crontab /tmp/cronfile
-      output
+    if [ $(grep -E "enable_ssl.sh" /tmp/cronfile | wc -l) -ne "0" ]; then
+        sed -i '/enable_ssl.sh/d' /tmp/cronfile
+        echo "00 03 */85 * * cd /tmp/ && ./enable_ssl.sh -d $domain -r" >> /tmp/cronfile
+        crontab /tmp/cronfile
+    else
+        echo "00 03 */85 * * cd /tmp/ && ./enable_ssl.sh -d $domain -r" >> /tmp/cronfile
+        crontab /tmp/cronfile
     fi
-    rm /tmp/cronfile
 
 }
 
