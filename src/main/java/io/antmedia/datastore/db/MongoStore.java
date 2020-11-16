3474f8ab088f6e5fe71ea13a3cfebc45895b5603
golgetahir <tahir.golge@gmail.com>
2020-11-16 11:32:02 +0300

    removed unused import

--- src/main/java/io/antmedia/datastore/db/MongoStore.java
+++ src/main/java/io/antmedia/datastore/db/MongoStore.java
@@ -14,7 +14,6 @@ import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mongodb.BasicDBObject;
 import com.mongodb.AggregationOptions;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
