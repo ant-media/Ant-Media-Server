c634a30b84c09a886ad6ad79e9b4a1f0e5893554
Selim <selimemretoy@gmail.com>
2020-12-01 01:57:56 +0300

    Add more tests about CorsHeaderFilter

--- src/test/java/io/antmedia/test/filter/CorsHeaderFilterTest.java
+++ src/test/java/io/antmedia/test/filter/CorsHeaderFilterTest.java
@@ -43,25 +43,54 @@ public class CorsHeaderFilterTest {
 		FilterChain filterChain =  Mockito.mock(FilterChain.class);
 		
 		{	
-			// http://localhost:4200
+			// Allow this request for angular app development
 			Mockito.when(request.getHeader(CorsFilter.REQUEST_HEADER_ORIGIN)).thenReturn("http://localhost:4200");
 			corsFilterSpy.addStandardHeadersInternal(request, response, "http://localhost:4200");
 			Mockito.verify(response).addHeader(CorsFilter.RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:4200");
 		}
 		{
-			// request.getHeader("Authorization")
+			request = Mockito.mock(HttpServletRequest.class);
+			response =  Mockito.mock(HttpServletResponse.class);
+			filterChain =  Mockito.mock(FilterChain.class);
+			
+			// request.getHeader("Authorization") have right header, so allow this request
 			Mockito.when(request.getHeader("Authorization")).thenReturn("JwtToken");
 			Mockito.when(request.getHeader(CorsFilter.REQUEST_HEADER_ORIGIN)).thenReturn("http://11.22.33.44:5080");
 			corsFilterSpy.addStandardHeadersInternal(request, response, "http://11.22.33.44:5080");
 			Mockito.verify(response).addHeader(CorsFilter.RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, "http://11.22.33.44:5080");
 		}
-		
 		{
-			//	(request.getHeader(REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS) != null && request.getHeader(REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS).contains("authorization")
+			request = Mockito.mock(HttpServletRequest.class);
+			response =  Mockito.mock(HttpServletResponse.class);
+			filterChain =  Mockito.mock(FilterChain.class);
+			
+			//	REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS have wrong header, so deny this request
+			Mockito.when(request.getHeader(CorsFilter.REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS)).thenReturn("otherHeader");
+			Mockito.when(request.getHeader(CorsFilter.REQUEST_HEADER_ORIGIN)).thenReturn("http://55.66.77.88:5080");
+			corsFilterSpy.addStandardHeadersInternal(request, response, "http://55.66.77.88:5080");
+			Mockito.verify(response).addHeader(CorsFilter.RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+		}
+		{
+			request = Mockito.mock(HttpServletRequest.class);
+			response =  Mockito.mock(HttpServletResponse.class);
+			filterChain =  Mockito.mock(FilterChain.class);
+			
+			//	REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS have right header, so allow this request
 			Mockito.when(request.getHeader(CorsFilter.REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS)).thenReturn("authorization");
 			Mockito.when(request.getHeader(CorsFilter.REQUEST_HEADER_ORIGIN)).thenReturn("http://55.66.77.88:5080");
 			corsFilterSpy.addStandardHeadersInternal(request, response, "http://55.66.77.88:5080");
 			Mockito.verify(response).addHeader(CorsFilter.RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, "http://55.66.77.88:5080");
+		}
+		{
+			request = Mockito.mock(HttpServletRequest.class);
+			response =  Mockito.mock(HttpServletResponse.class);
+			filterChain =  Mockito.mock(FilterChain.class);
+			
+			// REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS have null header, so deny this request
+			Mockito.when(request.getHeader(CorsFilter.REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS)).thenReturn(null);
+			Mockito.when(request.getHeader(CorsFilter.REQUEST_HEADER_ORIGIN)).thenReturn("http://55.66.77.88:5080");
+			corsFilterSpy.addStandardHeadersInternal(request, response, "http://55.66.77.88:5080");
+			Mockito.verify(response).addHeader(CorsFilter.RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, "*");
 		}
 		
 		//below codes for increasing coverage
