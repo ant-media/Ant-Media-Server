72b4b23e693793e40c0aa9c110cb5e012c367544
Selim <selimemretoy@gmail.com>
2021-02-09 22:22:19 +0300

    Add error log into analytic feature

---
+++ src/main/java/io/antmedia/logger/AntmediaAppender.java
@@ -0,0 +1,89 @@
+package io.antmedia.logger;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.red5.server.Launcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonObject;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.ThrowableProxyUtil;
+import ch.qos.logback.core.AppenderBase;
+import io.antmedia.statistic.StatsCollector;
+
+/**
+ * Appender for logback in charge of sending the logged events to a Firebase analytic server.
+ */
+public class AntmediaAppender extends AppenderBase<ILoggingEvent> {
+	
+	protected static final Logger logger = LoggerFactory.getLogger(AntmediaAppender.class);
+
+    @Override
+    public void append(ILoggingEvent iLoggingEvent) {
+        if (LoggerEnvironment.isManagingThread()) {
+            return;
+        }
+        LoggerEnvironment.startManagingThread();
+        try {
+            IThrowableProxy throwbleProxy = iLoggingEvent.getThrowableProxy();
+            if (throwbleProxy != null) {
+            	sendErrorToAnalytic(throwbleProxy);
+            }
+        } catch (Exception e) {
+            addError("An exception occurred", e);
+        } finally {
+            LoggerEnvironment.stopManagingThread();
+        }
+    }
+
+    @Override
+    public void stop() {
+        LoggerEnvironment.startManagingThread();
+        try {
+            if (!isStarted()) {
+                return;
+            }
+            super.stop();
+        } catch (Exception e) {
+            addError("An exception occurred", e);
+        } finally {
+            LoggerEnvironment.stopManagingThread();
+        }
+    }
+    
+	public void sendErrorToAnalytic(IThrowableProxy throwbleProxy) {
+		
+		String errorDetail = ThrowableProxyUtil.asString(throwbleProxy);
+		String instanceId = Launcher.getInstanceId();
+			
+		JsonObject instance = new JsonObject();
+		instance.addProperty(StatsCollector.INSTANCE_ID, instanceId);
+		instance.addProperty("errorDetail", errorDetail);
+
+		try (CloseableHttpClient client = getHttpClient()){
+			 HttpUriRequest post = RequestBuilder.post().setUri("https://us-central1-ant-media-server-analytics.cloudfunctions.net/sendErrorDetail").setHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+			.setEntity(new StringEntity(instance.toString())).build();
+			 
+			 client.execute(post);
+				
+			}catch (IOException e) {
+				logger.error("Couldn't connect Ant Media Server Analytics");
+			} 
+	}
+	
+	public static CloseableHttpClient getHttpClient() {
+		return  HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy())
+				.build();
+	}
+	
+}
---
+++ src/test/java/io/antmedia/test/logger/AntmediaAppenderTest.java
@@ -0,0 +1,65 @@
+package io.antmedia.test.logger;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import io.antmedia.logger.AntmediaAppender;
+import io.antmedia.logger.LoggerEnvironment;
+
+public class AntmediaAppenderTest {
+
+    private AntmediaAppender antmediaAppender;
+
+    @Before
+    public void before(){
+        LoggerEnvironment.stopManagingThread();
+        antmediaAppender = Mockito.spy(new AntmediaAppender());
+    }
+
+    @Test
+    public void logNotCalledWhenThrowbleProxyIsNull(){
+        ILoggingEvent iLoggingEvent = Mockito.mock(ILoggingEvent.class);
+        Mockito.when(iLoggingEvent.getThrowableProxy()).thenReturn(null);
+        antmediaAppender.append(iLoggingEvent);
+        Mockito.verify(antmediaAppender,Mockito.never()).sendErrorToAnalytic(Mockito.any(IThrowableProxy.class));
+    }
+
+    @Test
+    public void logCalledWhenThrowbleProxyIsNotNull(){
+        ILoggingEvent iLoggingEvent = Mockito.mock(ILoggingEvent.class);
+        Mockito.when(iLoggingEvent.getThrowableProxy()).thenReturn(Mockito.mock(IThrowableProxy.class));
+        antmediaAppender.append(iLoggingEvent);
+        Mockito.verify(antmediaAppender).sendErrorToAnalytic(Mockito.any(IThrowableProxy.class));
+    }
+
+    @Test
+    public void stopShouldStartManagingThread(){
+        antmediaAppender.stop();
+        Assert.assertNotNull(antmediaAppender);
+    }
+
+    @Test
+    public void constructorTest(){
+        AntmediaAppender appender = new AntmediaAppender();
+        Assert.assertNotNull(appender);
+    }
+
+    @Test
+    public void appenderShouldReturnWhenManagingThread(){
+        ILoggingEvent iLoggingEvent = Mockito.mock(ILoggingEvent.class);
+        Mockito.when(iLoggingEvent.getThrowableProxy()).thenReturn(Mockito.mock(IThrowableProxy.class));
+        LoggerEnvironment.startManagingThread();
+        antmediaAppender.append(iLoggingEvent);
+        Mockito.verify(antmediaAppender,Mockito.never()).sendErrorToAnalytic(Mockito.any(IThrowableProxy.class));
+    }
+
+    @Test
+    public void exceptionAddErrorTest(){
+        antmediaAppender.append(null);
+        Mockito.verify(antmediaAppender).addError(Mockito.anyString(),Mockito.any(Throwable.class));
+    }
+}
