7dc19a0fe7fb140f09adc459766c0cf33cc61f67
cem <cemkilic6@gmail.com>
2021-05-03 20:52:20 +0300

    decommented out some of storage clients

--- src/main/java/io/antmedia/AntMediaApplicationAdapter.java
+++ src/main/java/io/antmedia/AntMediaApplicationAdapter.java
@@ -1524,10 +1524,10 @@ public Result createInitializationProcess(String appName){
 		appSettings.setS3BucketName(newSettings.getS3BucketName());
 		appSettings.setS3RegionName(newSettings.getS3RegionName());
 
-		//storageClient.setStorageName(newSettings.getS3BucketName());
-		//storageClient.setAccessKey(newSettings.getS3AccessKey());
-		//storageClient.setSecretKey(newSettings.getS3SecretKey());
-		//storageClient.setRegion(newSettings.getS3RegionName());
+		storageClient.setStorageName(newSettings.getS3BucketName());
+		storageClient.setAccessKey(newSettings.getS3AccessKey());
+		storageClient.setSecretKey(newSettings.getS3SecretKey());
+		storageClient.setRegion(newSettings.getS3RegionName());
 
 		if (!appSettings.isS3RecordingEnabled()) {
 			appSettings.setS3AccessKey("");
