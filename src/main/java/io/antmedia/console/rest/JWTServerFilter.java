808287a127d152a0b0bd7fdbefffe79642d82771
cem <cemkilic6@gmail.com>
2021-07-26 14:25:27 +0300

    remove unnecessary imports and return statement

--- src/main/java/io/antmedia/console/rest/JWTServerFilter.java
+++ src/main/java/io/antmedia/console/rest/JWTServerFilter.java
@@ -21,7 +21,6 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-
 import io.antmedia.filter.AbstractFilter;
 
 
