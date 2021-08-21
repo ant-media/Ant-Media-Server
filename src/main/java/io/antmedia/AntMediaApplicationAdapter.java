9612db3d463b23e28212455a45159a213102b2f3
mekya <ahmetmermerkaya@gmail.com>
2021-08-21 20:42:01 +0300

    Fix test case

--- src/main/java/io/antmedia/AntMediaApplicationAdapter.java
+++ src/main/java/io/antmedia/AntMediaApplicationAdapter.java
@@ -1364,7 +1364,8 @@ public class AntMediaApplicationAdapter implements IAntMediaStreamHandler, IShut
 
 		boolean result = false;
 		
-		if (appSettings.getUpdateTime() != 0 && appSettings.getUpdateTime() > newSettings.getUpdateTime()) {
+		if (appSettings.getUpdateTime() != 0 && newSettings.getUpdateTime() != 0 
+				&& appSettings.getUpdateTime() > newSettings.getUpdateTime()) {
 			//if current app settings update time is bigger than the newSettings, don't update the bean
 			//it may happen in cluster mode, app settings may be updated locally then a new update just may come instantly from cluster settings.
 			logger.warn("Not saving the settings because current appsettings update time({}) is later than incoming settings update time({}) ", appSettings.getUpdateTime(), newSettings.getUpdateTime() );
