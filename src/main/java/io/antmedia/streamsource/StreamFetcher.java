04c54e25c1c9427469bed7e23ed8cee99e269198
Ahmet Oğuz Mermerkaya <ahmetmermerkaya@gmail.com>
2021-11-15 20:54:18 +0300

    Change Ms to Us for micro seconds

--- src/main/java/io/antmedia/streamsource/StreamFetcher.java
+++ src/main/java/io/antmedia/streamsource/StreamFetcher.java
@@ -154,8 +154,8 @@ public class StreamFetcher {
 		String timeoutStr = String.valueOf(this.timeout);
 		av_dict_set(optionsDictionary, "stimeout", timeoutStr, 0);
 
-		int analyzeDurationMs = appSettings.getMaxAnalyzeDurationMS() * 1000;
-		String analyzeDuration = String.valueOf(analyzeDurationMs);
+		int analyzeDurationUs = appSettings.getMaxAnalyzeDurationMS() * 1000;
+		String analyzeDuration = String.valueOf(analyzeDurationUs);
 		av_dict_set(optionsDictionary, "analyzeduration", analyzeDuration, 0);
 
 		int ret;
