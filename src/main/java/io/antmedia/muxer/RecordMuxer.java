c7c69829705331e5ca3dcd9882e5fd6eb11619ea
tahir <tahir.golge@gmail.com>
2021-12-06 15:18:00 +0300

    Remove unused log

--- src/main/java/io/antmedia/muxer/RecordMuxer.java
+++ src/main/java/io/antmedia/muxer/RecordMuxer.java
@@ -456,7 +456,6 @@ public abstract class RecordMuxer extends Muxer {
 
 	public File getFinalMp4FileName(boolean isS3Enabled){
 		String absolutePath = fileTmp.getAbsolutePath();
-		logger.info("88888888888888888888888888888888");
 		String origFileName = absolutePath.replace(TEMP_EXTENSION, "");
 
 		String prefix = s3FolderPath + File.separator + (subFolder != null ? subFolder + File.separator : "" );
