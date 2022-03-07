672a872e7bb1ecbccf1891c2848e04e3530e86e3
golgetahir <tahir.golge@gmail.com>
2022-03-07 13:42:12 +0300

    Refactor and fix output ID, search for the same output ID when the client stops

--- src/main/java/io/antmedia/webrtc/api/IWebRTCAdaptor.java
+++ src/main/java/io/antmedia/webrtc/api/IWebRTCAdaptor.java
@@ -120,4 +120,10 @@ public interface IWebRTCAdaptor extends IScopeService {
 	 */
 	void setTryCountBeforeSwitchback(int tryCountBeforeSwitchback);
 
+	/**
+	 * If there is a backup stream this method will find it and start it.
+	 * @param streamId
+	 */
+	void checkBackupStream(String streamId,  IWebRTCClient webRTCClient);
+
 }
