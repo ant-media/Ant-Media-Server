94c8a7f81adf2ecef29a542df6390e4d3585e6d5
burak-58 <burakkekec@gmail.com>
2022-06-03 15:18:01 +0300

    # WARNING: head commit changed in the meantime
    Merge branch 'master' into videoTrackOptimisation

--- src/main/java/io/antmedia/websocket/WebSocketConstants.java
+++ src/main/java/io/antmedia/websocket/WebSocketConstants.java
@@ -454,4 +454,30 @@ public class WebSocketConstants {
 	 */
 	public static final String UPDATE_STREAM_META_DATA_COMMAND = "updateStreamMetaData";
 
+	/**
+	 * Command to inform AMS if a stream is pinned in conference mode
+	 */
+	public static final String ASSIGN_VIDEO_TRACK_COMMAND = "assignVideoTrackCommand";
+	
+	/**
+	 * Command to change visible streams in conference mode, used for pagination
+	 */
+	public static final String UPDATE_VIDEO_TRACK_ASSIGNMENTS_COMMAND = "updateVideoTrackAssignmentsCommand";
+	
+	/**
+	 * Track id that is pinned for a stream
+	 */
+	public static final String VIDEO_TRACK_ID = "videoTrackId";
+	
+	/**
+	 * Start index of a list for pagination
+	 */
+	public static final String OFFSET = "offset";
+	
+	/**
+	 * Length of a page for pagination
+	 */
+	public static final String SIZE = "size";
+
+	
 }
