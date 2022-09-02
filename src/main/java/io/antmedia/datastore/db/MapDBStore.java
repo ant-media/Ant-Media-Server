35aea7579cee2ebcc3d11415e2ac66acf7b426e9
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2022-09-02 19:34:33 +0300

    Rename VoDIdStreamIdPairMap

--- src/main/java/io/antmedia/datastore/db/MapDBStore.java
+++ src/main/java/io/antmedia/datastore/db/MapDBStore.java
@@ -43,7 +43,7 @@ public class MapDBStore extends DataStore {
 	private DB db;
 	private BTreeMap<String, String> map;
 	private BTreeMap<String, String> vodMap;
-	private BTreeMap<String, String> VoDIdStreamIdPairMap;
+	private BTreeMap<String, String> voDIdStreamIdPairMap;
 	private BTreeMap<String, String> detectionMap;
 	private BTreeMap<String, String> tokenMap;
 	private BTreeMap<String, String> subscriberMap;
@@ -87,7 +87,7 @@ public class MapDBStore extends DataStore {
 		vodMap = db.treeMap(VOD_MAP_NAME).keySerializer(Serializer.STRING).valueSerializer(Serializer.STRING)
 				.counterEnable().createOrOpen();
 
-		VoDIdStreamIdPairMap = db.treeMap(VOD_ID_STREAM_ID_PAIR_MAP_NAME).keySerializer(Serializer.STRING).valueSerializer(Serializer.STRING)
+		voDIdStreamIdPairMap = db.treeMap(VOD_ID_STREAM_ID_PAIR_MAP_NAME).keySerializer(Serializer.STRING).valueSerializer(Serializer.STRING)
 				.counterEnable().createOrOpen();
 
 		detectionMap = db.treeMap(DETECTION_MAP_NAME).keySerializer(Serializer.STRING)
@@ -464,18 +464,18 @@ public class MapDBStore extends DataStore {
 
 	@Override
 	public Optional<String> getVoDId(String streamId) {
-		return Optional.ofNullable(VoDIdStreamIdPairMap.get(streamId));
+		return Optional.ofNullable(voDIdStreamIdPairMap.get(streamId));
 	}
 
 	@Override
 	public boolean saveVoDId(String streamId, String voDId) {
-		VoDIdStreamIdPairMap.put(streamId, voDId);
+		voDIdStreamIdPairMap.put(streamId, voDId);
 		return true;
 	}
 
 	@Override
 	public void removeVoDIdByStreamId(String streamId) {
-		VoDIdStreamIdPairMap.remove(streamId);
+		voDIdStreamIdPairMap.remove(streamId);
 	}
 
 	/**
