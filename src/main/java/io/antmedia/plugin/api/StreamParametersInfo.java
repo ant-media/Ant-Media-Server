4c5f3b884799a823f00dfab00922e57ea4564658
burak-58 <burakkekec@gmail.com>
2022-10-26 13:55:56 +0300

    inform filter if stream is hosted by another node

--- src/main/java/io/antmedia/plugin/api/StreamParametersInfo.java
+++ src/main/java/io/antmedia/plugin/api/StreamParametersInfo.java
@@ -6,5 +6,6 @@ import org.bytedeco.ffmpeg.avutil.AVRational;
 public class StreamParametersInfo {
 	public AVCodecParameters codecParameters;
 	public AVRational timeBase;
-	public boolean enabled; 
+	public boolean enabled;
+	public boolean hostedInOtherNode = false; //set true if the stream is hosted by another node in cluster mode
 }
