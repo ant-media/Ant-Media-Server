cbb03c36c4b0184765b4941396d175e208293e47
Murat Ugur Eminoglu <murat@murat.ws>
2023-09-11 12:01:21 +0300

    Fix AWS metadata URL

--- src/main/server/enable_ssl.sh
+++ src/main/server/enable_ssl.sh
@@ -160,10 +160,10 @@ get_freedomain(){
 
   check_marketplace() {
     #AWS
-    if curl -s http://169.254.169.254/latest/meta-data/ >/dev/null; then
+    if curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" >/dev/null; then
       return 0
     #AZURE
-    elif curl -H Metadata:true -s http://169.254.169.254/metadata/instance?api-version=2021-02-01 >/dev/null; then
+    elif curl -H Metadata:true -s "http://169.254.169.254/metadata/instance?api-version=2021-02-01 >/dev/null"; then
       return 0
     #GCP
     elif curl -H "Metadata-Flavor: Google" -s http://metadata.google.internal/computeMetadata/v1/?recursive=true >/dev/null; then
