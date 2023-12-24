2f38a6a6542a773d9e561fed8be2b20f884934d6
mekya <ahmetmermerkaya@gmail.com>
2023-12-24 18:41:12 +0300

    Wait app creation for 20 seconds in createApplication REST method

--- src/main/java/io/antmedia/console/rest/RestServiceV2.java
+++ src/main/java/io/antmedia/console/rest/RestServiceV2.java
@@ -6,22 +6,10 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
-
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.red5.server.api.scope.IScope;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
@@ -42,6 +30,18 @@ import io.swagger.annotations.ExternalDocs;
 import io.swagger.annotations.Info;
 import io.swagger.annotations.License;
 import io.swagger.annotations.SwaggerDefinition;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 
 
@@ -57,7 +57,7 @@ import io.swagger.annotations.SwaggerDefinition;
 		produces = {"application/json"},
 		schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS},
 		externalDocs = @ExternalDocs(value = "External Docs", url = "https://antmedia.io")
-)
+		)
 @Component
 @Path("/v2")
 public class RestServiceV2 extends CommonRestService {
@@ -499,12 +499,12 @@ public class RestServiceV2 extends CommonRestService {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Override
 	public Result configureSsl(@ApiParam(value = "SSL settings", required = true) @QueryParam("domain") String domain, @QueryParam("type") String type,
-							   @FormDataParam("fullChainFile") InputStream fullChainFile,
-							   @FormDataParam("fullChainFile") FormDataContentDisposition fullChainFileDetail,
-							   @FormDataParam("privateKeyFile") InputStream privateKeyFile,
-							   @FormDataParam("privateKeyFile") FormDataContentDisposition privateKeyFileDetail,
-							   @FormDataParam("chainFile") InputStream chainFile,
-							   @FormDataParam("chainFile") FormDataContentDisposition chainFileDetail)
+			@FormDataParam("fullChainFile") InputStream fullChainFile,
+			@FormDataParam("fullChainFile") FormDataContentDisposition fullChainFileDetail,
+			@FormDataParam("privateKeyFile") InputStream privateKeyFile,
+			@FormDataParam("privateKeyFile") FormDataContentDisposition privateKeyFileDetail,
+			@FormDataParam("chainFile") InputStream chainFile,
+			@FormDataParam("chainFile") FormDataContentDisposition chainFileDetail)
 
 	{
 		return super.configureSsl(domain, type, fullChainFile, fullChainFileDetail, privateKeyFile, privateKeyFileDetail, chainFile, chainFileDetail);
@@ -597,7 +597,7 @@ public class RestServiceV2 extends CommonRestService {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Override
 	public String getLogFile(@ApiParam(value = "Char size of the log", required = true) @PathParam("charSize") int charSize, @ApiParam(value = "Log type. ERROR can be used to get only error logs", required = true) @QueryParam("logType") String logType,
-							 @ApiParam(value = "Offset of the retrieved log", required = true) @PathParam("offsetSize") long offsetSize) throws IOException {
+			@ApiParam(value = "Offset of the retrieved log", required = true) @PathParam("offsetSize") long offsetSize) throws IOException {
 		return super.getLogFile(charSize,logType, offsetSize);
 	}
 
@@ -630,21 +630,49 @@ public class RestServiceV2 extends CommonRestService {
 		Result result;
 		if (appName != null && appName.matches("^[a-zA-Z0-9]*$"))
 		{
-			List<String> applications = getApplication().getApplications();
 
 			boolean applicationAlreadyExist = false;
-			for (String applicationName : applications)
-			{
-				if (applicationName.equalsIgnoreCase(appName))
-				{
-					applicationAlreadyExist = true;
-					break;
-				}
+
+			IScope scope = getApplication().getRootScope().getScope(appName);
+			if (scope != null) {
+				applicationAlreadyExist = true;
 			}
 
 			if (!applicationAlreadyExist)
 			{
 				result = super.createApplication(appName, inputStream);
+				if (result.isSuccess()) 
+				{
+
+					boolean applicationCreated = false;
+					for (int i = 0; i < 20; i++) 
+					{
+						try {
+							Thread.sleep(1000);
+
+							scope = getApplication().getRootScope().getScope(appName);
+							if (scope != null) {
+								applicationCreated = true;
+								break;
+							}
+
+
+						} 
+						catch (InterruptedException e) {
+							logger.error(ExceptionUtils.getStackTrace(e));
+						}
+					}
+					
+					
+					if (!applicationCreated) {
+						result.setSuccess(applicationCreated);
+						result.setMessage("Application " + appName + "is not created in the 20 seconds.");
+					}
+
+
+
+				}
+
 			}
 			else
 			{
@@ -666,7 +694,7 @@ public class RestServiceV2 extends CommonRestService {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Override
 	public Result deleteApplication(@ApiParam(value = "Name of the application to delete", required = true) @PathParam("appName") String appName,
-									@QueryParam("deleteDB") boolean deleteDB) {
+			@QueryParam("deleteDB") boolean deleteDB) {
 		if (appName != null) {
 			return super.deleteApplication(appName, deleteDB);
 		}
