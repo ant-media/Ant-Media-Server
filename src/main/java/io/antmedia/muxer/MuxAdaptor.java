ca403e2779b61fd9c1b9ecabd9f1f31aa3f32205
mekya <ahmetmermerkaya@gmail.com>
2025-02-20 16:33:32 +0300

    Close connection if it's not updating

--- src/main/java/io/antmedia/muxer/MuxAdaptor.java
+++ src/main/java/io/antmedia/muxer/MuxAdaptor.java
@@ -1362,11 +1362,7 @@ public class MuxAdaptor implements IRecordingListener, IEndpointStatusListener {
 
 					if (stopRequestExist) {
 						logger.info("Stop request exists for stream:{}", streamId);
-						broadcastStream.removeStreamListener(MuxAdaptor.this);
-						logger.warn("closing adaptor for {} ", streamId);
-						closeResources();
-						logger.warn("closed adaptor for {}", streamId);
-						getStreamHandler().stopPublish(streamId);
+						clearAndStopStream();
 						//finally code execute and reset the isPipeReaderJobRunning
 						return;
 
@@ -1439,7 +1435,6 @@ public class MuxAdaptor implements IRecordingListener, IEndpointStatusListener {
 
 					updateQualityParameters(dts, TIME_BASE_FOR_MS, 0, isKeyFrame);
 					
-
 					if (bufferTimeMs == 0) 
 					{
 						writeStreamPacket(packet);
@@ -1450,13 +1445,21 @@ public class MuxAdaptor implements IRecordingListener, IEndpointStatusListener {
 					}
 
 				}
+				
+				long now = System.currentTimeMillis();
+				if ((now - lastQualityUpdateTime) > AntMediaApplicationAdapter.STREAM_TIMEOUT_MS) 
+				{
+					//It's not updated for timeout period, it means that stream is not sending packets and it is accepted as offline
+					//close Rtmp Connection
+					logger.info("Closing Rtmp Connection because it's not updated for {} ms for stream:{}", AntMediaApplicationAdapter.STREAM_TIMEOUT_MS, streamId);
+					closeRtmpConnection();
+					stopRequestExist = true;
+				}
+				
 
 				if (stopRequestExist) {
-					broadcastStream.removeStreamListener(MuxAdaptor.this);
-					logger.warn("closing adaptor for {} ", streamId);
-					closeResources();
-					logger.warn("closed adaptor for {}", streamId);
-					getStreamHandler().stopPublish(streamId);
+					logger.info("Stop request exists for stream:{}", streamId);
+					clearAndStopStream();
 				}	
 			}
 			finally {
@@ -1465,6 +1468,14 @@ public class MuxAdaptor implements IRecordingListener, IEndpointStatusListener {
 				isPipeReaderJobRunning.compareAndSet(true, false);
 			}
 		}
+	}
+	
+	public void clearAndStopStream() {
+		broadcastStream.removeStreamListener(MuxAdaptor.this);
+		logger.warn("closing adaptor for {} ", streamId);
+		closeResources();
+		logger.warn("closed adaptor for {}", streamId);
+		getStreamHandler().stopPublish(streamId);
 	}
 
 
