891a19cc8e41df76ebfa15c965d8f4212f3d2929
burak <burak@antmedia.io>
2025-03-04 12:42:43 +0300

    add audio level threshold settings parameter

--- src/main/java/io/antmedia/AppSettings.java
+++ src/main/java/io/antmedia/AppSettings.java
@@ -2290,6 +2290,14 @@ public class AppSettings implements Serializable{
 	 */
 	@Value("${sendAudioLevelToViewers:false}")
 	private boolean sendAudioLevelToViewers = false;
+	
+	/**
+	 *  
+	 * Audio level threshold to assign an audio track to stream in case of limited audio tracks in conference
+	 * 
+	 */
+	@Value("${audioLevelThreshold:90}")
+	private int audioLevelThreshold = 90;
 
 	/**
 	 * Enable/disable video frame scaling in GPU when there is an adaptive bitrate.
@@ -4272,5 +4280,13 @@ public class AppSettings implements Serializable{
 	 */
 	public void setHlsSegmentFileSuffixFormat(String hlsSegmentFileSuffixFormat) {
 		this.hlsSegmentFileSuffixFormat = hlsSegmentFileSuffixFormat;
+	}
+
+	public int getAudioLevelThreshold() {
+		return audioLevelThreshold;
+	}
+
+	public void setAudioLevelThreshold(int audioLevelThreshold) {
+		this.audioLevelThreshold = audioLevelThreshold;
 	}
 }
