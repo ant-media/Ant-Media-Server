7b655c10796a02b1896c17282dcce5043428a1be
USAMAWIZARD <usamawizard@gmail.com>
2025-03-19 21:22:12 +0530

    add option/disable rtmps from server settings

---
+++ src/main/java/org/red5/server/net/rtmp/MockRTMPMinaIoHandler.java
@@ -0,0 +1,86 @@
+package org.red5.server.net.rtmp;
+
+import java.util.Set;
+
+import org.apache.mina.core.service.IoHandler;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MockRTMPMinaIoHandler extends IoHandlerAdapter {
+
+  private static Logger log = LoggerFactory.getLogger(MockRTMPMinaIoHandler.class);
+
+  /**
+   * Mock RTMP events handler
+   */
+  protected IRTMPHandler handler;
+
+  private String keystorePassword;
+
+  private String keystoreFile;
+
+  private String truststorePassword;
+
+  private String truststoreFile;
+
+  private String cipherSuites[];
+
+  private String protocols[];
+
+  private IoHandler ioHandler;
+
+  protected Set<String> addresses;
+
+  protected int ioThreads;
+
+  protected boolean tcpNoDelay;
+
+  public void setHandler(IRTMPHandler handler) {
+    this.handler = handler;
+  }
+
+  public void setKeystorePassword(String keystorePassword) {
+    this.keystorePassword = keystorePassword;
+  }
+
+  public void setKeystoreFile(String keystoreFile) {
+    this.keystoreFile = keystoreFile;
+  }
+
+  public void setTruststorePassword(String truststorePassword) {
+    this.truststorePassword = truststorePassword;
+  }
+
+  public void setTruststoreFile(String truststoreFile) {
+    this.truststoreFile = truststoreFile;
+  }
+
+  public void setCipherSuites(String[] cipherSuites) {
+    this.cipherSuites = cipherSuites;
+  }
+
+  public void setProtocols(String[] protocols) {
+    this.protocols = protocols;
+  }
+
+  public void setIoHandler(IoHandler ioHandler) {
+    this.ioHandler = ioHandler;
+  }
+
+  public void setAddresses(Set<String> addresses) {
+    this.addresses = addresses;
+  }
+
+  public void setIoThreads(int ioThreads) {
+    this.ioThreads = ioThreads;
+  }
+
+  public void setTcpNoDelay(boolean tcpNoDelay) {
+    this.tcpNoDelay = tcpNoDelay;
+  }
+
+  public void start(){}
+
+  public void stop(){}
+}
