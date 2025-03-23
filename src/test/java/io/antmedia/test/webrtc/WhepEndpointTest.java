2ea34e54d5aef534908c578722786f0253f3e6eb
golgetahir <tahir.golge@gmail.com>
2025-03-23 15:04:04 +0300

    Fix import on test

--- src/test/java/io/antmedia/test/webrtc/WhepEndpointTest.java
+++ src/test/java/io/antmedia/test/webrtc/WhepEndpointTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
+import static org.junit.Assert.assertNotNull;
 
 import java.net.URI;
 import java.util.concurrent.CompletableFuture;
