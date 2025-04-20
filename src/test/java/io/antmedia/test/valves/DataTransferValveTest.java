1e2d48f90159a8cbac1f5fda12513ffc0031bdec
burak <burak@antmedia.io>
2025-04-21 01:53:28 +0300

    fix test cases

--- src/test/java/io/antmedia/test/valves/DataTransferValveTest.java
+++ src/test/java/io/antmedia/test/valves/DataTransferValveTest.java
@@ -17,6 +17,7 @@ import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
 
 import io.antmedia.AntMediaApplicationAdapter;
+import io.antmedia.AppSettings;
 import io.antmedia.analytic.model.PlayerStatsEvent;
 import io.antmedia.filter.TokenFilterManager;
 import io.antmedia.logger.LoggerUtils;
@@ -58,6 +59,8 @@ public class DataTransferValveTest {
         when(request.getServletContext()).thenReturn(servletContext);
         when(request.getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE)).thenReturn(context);
         when(context.getBean(AntMediaApplicationAdapter.BEAN_NAME)).thenReturn(appAdapter);
+        when(context.getBean(AppSettings.BEAN_NAME)).thenReturn(new AppSettings());
+
         
         when(appAdapter.getScope()).thenReturn(Mockito.mock(IScope.class));
         when(appAdapter.getScope().getName()).thenReturn("appScope");
