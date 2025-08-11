2aaca2d3a03c63f3d940a83e5f8a849c14019ed3
golgetahir <tahir.golge@gmail.com>
2025-08-11 10:22:00 +0300

    Fix number of app settings in test

--- src/test/java/io/antmedia/test/AppSettingsUnitTest.java
+++ src/test/java/io/antmedia/test/AppSettingsUnitTest.java
@@ -695,7 +695,7 @@ public class AppSettingsUnitTest extends AbstractJUnit4SpringContextTests {
 		//by also checking its default value. 
 
 		assertEquals("New field is added to settings. PAY ATTENTION: Please CHECK ITS DEFAULT VALUE and fix the number of fields.", 
-				206, numberOfFields);
+				204, numberOfFields);
 	}
 
 
