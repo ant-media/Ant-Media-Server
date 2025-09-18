9d8c71c352dc3df702b4b80c9bdb7d714178e22b
master312 <sythinian@gmail.com>
2025-09-18 22:57:57 +0200

    Reworked cluster exposure interface.

--- src/main/java/io/antmedia/cluster/IHttpLiveStreamValve.java
+++ src/main/java/io/antmedia/cluster/IHttpLiveStreamValve.java
@@ -1,17 +1,18 @@
 package io.antmedia.cluster;
 
+import io.antmedia.AppSettings;
 import io.antmedia.servlet.IChunkedCacheManager;
 import io.vertx.core.Vertx;
 
 public interface IHttpLiveStreamValve {
-	public static final String M4S_EXTENSION = ".m4s";
+	static final String M4S_EXTENSION = ".m4s";
 
-	public static final String M3U8_EXTENSION = ".m3u8";
+	static final String M3U8_EXTENSION = ".m3u8";
 
-	public static final String TS_EXTENSION = ".ts";
+	static final String TS_EXTENSION = ".ts";
 
-	public static final String MPD_EXTENSION = ".mpd";
+	static final String MPD_EXTENSION = ".mpd";
 
 
-	void downloadRequestedFile(RequestInfo reqInfo, String hostName, Vertx vertx, IChunkedCacheManager iChunkedCacheManager, String jwtSecretKey);
+	void downloadRequestedFileFromUrl(String url, AppSettings appSettings, String hostName, Vertx vertx, IChunkedCacheManager iChunkedCacheManager, String jwtSecretKey);
 }
