4968b99deda22aaa0d628c736e13482c76304bfd
golgetahir <tahir.golge@gmail.com>
2025-12-21 13:21:01 +0300

    Fix unit tests

--- src/test/java/io/antmedia/test/AppSettingsUnitTest.java
+++ src/test/java/io/antmedia/test/AppSettingsUnitTest.java
@@ -690,12 +690,14 @@ public class AppSettingsUnitTest extends AbstractJUnit4SpringContextTests {
 		assertEquals("", appSettings.getStreamEndedScript());
 		assertEquals("", appSettings.getStreamIdleTimeoutScript());
 
+		assertEquals("[]", appSettings.getIceServers());
+
 		//if we add a new field, we just need to check its default value in this test
 		//When a new field is added or removed please update the number of fields and make this test pass
 		//by also checking its default value. 
 
 		assertEquals("New field is added to settings. PAY ATTENTION: Please CHECK ITS DEFAULT VALUE and fix the number of fields.", 
-				204, numberOfFields);
+				205, numberOfFields);
 	}
 
 
