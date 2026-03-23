034237b17a573040fb17b886ecfddcfd7df39327
Ahmet Oguz Mermerkaya <mekya@antmedia.io>
2026-03-23 09:45:49 +0300

    Fix test errors

--- src/test/java/io/antmedia/test/AppSettingsUnitTest.java
+++ src/test/java/io/antmedia/test/AppSettingsUnitTest.java
@@ -673,7 +673,7 @@ public class AppSettingsUnitTest extends AbstractJUnit4SpringContextTests {
 		//by also checking its default value. 
 
 		assertEquals("New field is added to settings. PAY ATTENTION: Please CHECK ITS DEFAULT VALUE and fix the number of fields.", 
-				186, numberOfFields);
+				182, numberOfFields);
 	}
 
 
