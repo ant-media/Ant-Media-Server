a4cfd58e9e55c55cd8ac698e32123f3b00986b4e
golgetahir <tahir.golge@gmail.com>
2026-07-13 11:31:12 +0300

    Fix test

--- src/test/java/io/antmedia/test/webrtc/adaptor/RTMPAdaptorTest.java
+++ src/test/java/io/antmedia/test/webrtc/adaptor/RTMPAdaptorTest.java
@@ -1,6 +1,10 @@
 package io.antmedia.test.webrtc.adaptor;
 
 import static org.bytedeco.ffmpeg.global.avutil.AV_PIX_FMT_YUV420P;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
@@ -42,8 +46,23 @@ import io.antmedia.websocket.WebSocketConstants;
 import jakarta.websocket.RemoteEndpoint;
 import jakarta.websocket.Session;
 
+@Tag("fast")
 public class RTMPAdaptorTest {
 
 	//moved to enterprise because it's crashing time to time in travis ci while loading the lib and I could not resolve
 	//mekya
+
+	@Test
+	public void testSetStunServerUri() {
+		//construction and setters don't touch the native webrtc lib, so they're safe to run here
+		RTMPAdaptor adaptor = new RTMPAdaptor("rtmp://localhost/live/test", null, 480);
+
+		//4-arg overload also stores the iceServers config
+		adaptor.setStunServerUri("stun:stun1.l.google.com:19302", "user", "cred", "[{\"urls\":\"turn:turn.antmedia.io\"}]");
+		assertEquals("stun:stun1.l.google.com:19302", adaptor.getStunServerUri());
+
+		//3-arg overload delegates with a null iceServers config
+		adaptor.setStunServerUri("stun:stun2.l.google.com:19302", "user2", "cred2");
+		assertEquals("stun:stun2.l.google.com:19302", adaptor.getStunServerUri());
+	}
 }
